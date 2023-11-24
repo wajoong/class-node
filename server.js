dd420808ac5e3b6440af3afe29a9ce7e1db47397
wajoong <kjuseong4121@naver.com>
2023-11-24 16:51:41 +0900

    firstcommint

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+app.listen("8080", (req,res) => {
+    console.log("포트 8080으로 서버 대기중...")
+})
+app.get("/book", (req,res) =>{
+    console.log("책방입니다.");
+})
